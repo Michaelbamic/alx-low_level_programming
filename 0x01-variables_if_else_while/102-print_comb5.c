9cad74102235e7aea77e2eda7c6a214ed0239c0d
Michaelbamic <michaelbamic4shot@gmail.com>
2022-10-14 08:45:04 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,9 +12,9 @@ int main(void)
 {
 	int d, f;
 
-	for (d = '0'; d <= '98'; d++)
+	for (d = 0; d <= 98; d++)
 	{
-		for (f = d + 1; f <= '99'; f++)
+		for (f = d + 1; f <= 99; f++)
 		{
 			putchar((d / 10) + '0');
 			putchar((d % 10) + '0');
@@ -22,7 +22,7 @@ int main(void)
 			putchar((f / 10) + '0');
 			putchar((f % 10) + '0');
 
-			if (d == '98' && f == '99')
+			if (d == 98 && f == 99)
 				continue;
 
 			putchar(',');
