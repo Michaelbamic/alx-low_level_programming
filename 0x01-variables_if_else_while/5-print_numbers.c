f25e8f5c56d09b83b6ba5f898a61db692fb431a3
Michaelbamic <michaelbamic4shot@gmail.com>
2022-10-14 07:16:12 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
 * main - Entry point
@@ -16,7 +14,7 @@ int main(void)
 	{
 		printf("%d", i);
 	}
-	putchar("\n");
+	printf("\n");
 
 	return (0);
 }
