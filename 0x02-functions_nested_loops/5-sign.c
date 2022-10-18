15cd7851cdec00df7b5b3e47ec63aa27fb90b19c
Michaelbamic <michaelbamic4shot@gmail.com>
2022-10-18 06:49:40 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,23 +10,22 @@
  */
 int print_sign(int n)
 {
-	int value;
 
 	if (n > 0)
 	{
-		value = 1;
-		_putchar('+');
+		_putchar(43);
+		return (1);
 	}
-	else if (n == 0)
+	else if (n < 0)
 	{
-		value = 0;
-		_putchar('0');
+		_putchar(45);
+		return (-1);
 	}
 	else
 	{
-		value = -1;
-		_putchar('-1');
+		_putchar(48);
+		return (0);
 	}
 
-	return (value);
+	_putchar('\n');
 }
