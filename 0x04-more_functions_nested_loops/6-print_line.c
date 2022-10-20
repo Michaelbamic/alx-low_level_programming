e40eedc6673e3959426de118388e81f98a255834
Michaelbamic <michaelbamic4shot@gmail.com>
2022-10-20 04:55:15 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,9 +10,13 @@ void print_line(int n)
 {
 	int i = 0;
 
-	if (n > 0)
+	if (n <= 0)
 	{
-		for (; i < n; i++)
+		_putchar('\n')
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
 		{
 			_putchar('_');
 		}
