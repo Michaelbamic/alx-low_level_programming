28e1bc09be076a78c1fa6d57119cde70629c46d5
Michaelbamic <michaelbamic4shot@gmail.com>
2022-10-20 06:11:15 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,16 +11,16 @@ int main(void)
 
 	for (; i < 100; i++)
 	{
-		if (1 % 3 == 0 && i % 5 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
 		else if (i % 3 == 0)
 			printf("Fizz ");
 		else if (i % 5 == 0)
 			printf("Buzz ");
 		else
-			printf(" %d ", i);
+			printf("%d ", i);
 	}
-	printf("\n");
+	printf("Buzz\n");
 
 	return (0);
 }
