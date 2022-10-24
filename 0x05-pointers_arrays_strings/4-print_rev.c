dc42f6215371d9dd50470813cf72554ff47cb5d5
Michaelbamic <michaelbamic4shot@gmail.com>
2022-10-24 06:21:17 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,14 +9,11 @@ void print_rev(char *s)
 {
 	int len = 0;
 
-	while (len >= 0)
-	{
-		if (s[len] == '\0')
-			break;
-		len++;
-	}
+	while (s[len] != '\0')
+		len++
 
-	for (len--; len >= 0; len--)
-		_putchar(s[len]);
+	while (len)
+		_putchar(s[--len]);
+
 	_putchar('\n');
 }
