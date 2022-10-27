35bcc03e9df3d5350409e51ec7df706b21b63fe4
Michaelbamic <michaelbamic4shot@gmail.com>
2022-10-27 00:10:51 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,14 +8,20 @@
  */
 char *_strcat(char *dest, char *src);
 {
-	char *len = dest;
+	int i = 0, j = 0;
 
-	while (*dest)
-		dest++;
+	while (*(dest + i) != '\0')
+	{
+		i++;
+	}
 
-	while (*src)
-		*dest++ = *src++;
-
-	*dest = '\0';
-	return (len);
+	while (j >= 0)
+	{
+		*(dest + i) = *(src + j);
+		if (*(src + j) == '\0')
+			break;
+		i++;
+		j++;
+	}
+	return (dest);
 }
