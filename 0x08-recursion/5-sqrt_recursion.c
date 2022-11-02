e20d300586a1056aae4a1bee49528e5dbc70c65a
Michaelbamic <michaelbamic4shot@gmail.com>
2022-11-02 03:48:40 -0700

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#define "main.h"
+/**
+ * power_operation - returns the natural square root of a number
+ * @n: input integer
+ * @c: iterator
+ * Return: square root or -1
+ */
+int power_operation(int n, int c)
+{
+	if (c % (c / n) == 0)
+	{
+		if (c * (n / c) == n)
+			return (c);
+		else
+			return (-1);
+	}
+	return (0 + power_operation(n, c + 1));
+}
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: input number
+ * Return: natural square root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (power_opreation(n, 2));
+}
