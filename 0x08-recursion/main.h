334eb5207fc68d8ed5daf2a4124e16ac7fbb04c1
Michaelbamic <michaelbamic4shot@gmail.com>
2022-11-02 05:23:49 -0700

    100-is_palindrome.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,5 +9,8 @@ int factorial(int n);
 int power_operation(int n, int c);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+
 
 #endif
