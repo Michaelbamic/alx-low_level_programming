7afdf55837983aa65040b9090aaaee9c4f38c2fd
Michaelbamic <michaelbamic4shot@gmail.com>
2022-11-02 03:21:03 -0700

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -6,7 +6,7 @@
  * @y: input power integer
  * Return: no return
  */
-int _pow_recursion(int x, int y);
+int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
@@ -15,5 +15,5 @@ int _pow_recursion(int x, int y);
 	else if (y == 1)
 		return (x);
 	else
-		return (x *= _pow_recursion(x, y - 1));
+		return (x * _pow_recursion(x, y - 1));
 }
