ec72f19c86a369db885e4f5e5dcee1711a333589
Michaelbamic <michaelbamic4shot@gmail.com>
2022-11-04 02:48:39 -0700

    my_static_libraries

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - Check Main
+ * @c: An input character
+ * Description: function uses _putchar function to print
+ * alphabet in lowercase 10 times
+ * Return: 1 if is a lowercase or 0 if is a uppercse
+ */
+int _islower(int c)
+{
+	char i;
+	int lower = 0;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == c)
+			lower = 1;
+	}
+	return (lower);
+}
