9077b87d7df7c66952b030bd68a71efede71db0a
Michaelbamic <michaelbamic4shot@gmail.com>
2022-11-10 07:01:08 -0800

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -16,12 +16,8 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(98);
 	}
-	/*else if (!isdigit(argv[1]) && isdigit(argv[2]))
-	{
-		printf("Error\n");
-		exit(98);
-	}*/
-	else
+
+	else 
 	{
 		num1 = atoi(argv[1]);
 		num2 = atoi(argv[2]);
@@ -29,4 +25,5 @@ int main(int argc, char **argv)
 		printf("%d\n", result);
 		return (0);
 	}
+	
 }
