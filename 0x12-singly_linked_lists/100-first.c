ea6bb53e48cd0db95214f4118df9520311cf7fa4
Michaelbamic <michaelbamic4shot@gmail.com>
2022-11-24 05:31:28 -0800

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * bfmain - function executed before main
+ * Return: no return
+ */
+
+void __attribute__ ((constructor)) bfmain()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
